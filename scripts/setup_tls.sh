62cb1533969515f472370eaac246f8b66a8f649d
Phil Eaton <phil@eatonphil.com>
2021-06-11 19:17:51 +0000

    Sync with certbot managed nginx

--- scripts/setup_tls.sh
+++ scripts/setup_tls.sh
@@ -1,4 +1,2 @@
-sudo wget https://dl.eff.org/certbot-auto -O /usr/sbin/certbot-auto
-sudo chmod a+x /usr/sbin/certbot-auto
-sudo certbot-auto --os-packages-only
-sudo certbot-auto --nginx
+sudo dnf install -y certbot certbot-nginx
+sudo certbot --nginx
