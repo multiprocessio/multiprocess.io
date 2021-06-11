7b54c1be35c3a1e6841b7a87db5ca9a0a14ac885
Phil Eaton <phil@eatonphil.com>
2021-06-11 19:06:22 +0000

    First commit

---
+++ scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -eux
+
+git clone git@gitlab.com/eatonphil:data-ide workspace || true
+( cd workspace && yarn && yarn build-ui )
+
+echo "
+<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-0YH69RMKWK\"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0YH69RMKWK');
+</script>" >> workspace/build/index.html
+
+REMOTE=fedora@datastation.multiprocess.io
+REMOTE_HOME=/home/fedora
+
+function remote_copy () {
+    scp -i ~/.ssh/id_rsa -r $1 $REMOTE:$2
+}
+
+function remote_run () {
+    ssh $REMOTE -- "$1"
+}
+
+remote_run "rm -rf $REMOTE_HOME/ui"
+remote_copy workspace/build $REMOTE_HOME/ui
+remote_copy site $REMOVE_HOME/site
+remote_copy config/nginx.conf $REMOTE_HOME/nginx.conf
+remote_copy config/selinux.conf $REMOTE_HOME/selinux.conf
+remote_run "sudo dnf install -y nginx && sudo mkdir -p /run && sudo mkdir -p /usr/share/nginx/logs && sudo mv $REMOTE_HOME/nginx.conf /etc/nginx && sudo nginx -t && sudo setenforce permissive && sudo service nginx restart && sudo mv $REMOTE_HOME/selinux.conf /etc/selinux/config"
+
+if [[ "$2" == "--setup-tls" ]]; then
+    remote_run "
+    sudo wget https://dl.eff.org/certbot-auto -O /usr/sbin/certbot-auto &&
+    sudo chmod a+x /usr/sbin/certbot-auto &&
+    sudo certbot-auto --os-packages-only
+    sudo certbot-auto --nginx"
+fi
