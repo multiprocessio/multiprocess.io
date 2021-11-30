81e948670ab7fc17ad3b1275183b06ca638007fb
Phil Eaton <phil@eatonphil.com>
2021-11-30 21:14:42 +0000

    Fetch stars automatically

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -9,3 +9,7 @@ python3 -m venv .env
 .env/bin/pip install -r requirements.txt
 .env/bin/python ./scripts/build_site.py
 cp assets/* build
+
+# Update stars count
+stars="$(curl -L https://api.github.com/repos/multiprocessio/datastation | jq '.stargazers_count')"
+sed -i 's/STARS/'"$stars"'/g' build/stars.html
