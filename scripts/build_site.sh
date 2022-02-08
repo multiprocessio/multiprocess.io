468a1ce043875d6442b6a686b6f0f6128f14f46b
Phil Eaton <phil@eatonphil.com>
2022-02-08 00:48:55 +0000

    cp -r

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -12,7 +12,7 @@ rm -rf build && mkdir -p build/blog build/docs
 python3 -m venv .env
 .env/bin/pip install -r requirements.txt
 .env/bin/python ./scripts/build_site.py
-cp assets/* build
+cp -r assets/* build
 
 if [[ "$1" != "--skip-stars" ]]; then
     # Update stars count
